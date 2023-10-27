57d395d1cbb4f93843876f90474aebde8323c6eb
ecoricemon <ecoricemon@gmail.com>
2023-10-27 17:27:22 +0900

    add: generic-trait-object

---
+++ generic-trait-object/src/main.rs
@@ -0,0 +1,166 @@
+//! # Trait object from trait with generic methods
+//!
+//! ## Situation
+//!
+//! - Want to make a trait obejct from a trait having some generic methods.
+//! - Generic methods require 'static lifetime such as `foo<T: 'static>()`.
+//!
+//! ## Pattern abstration
+//!
+//! In Rust, only object safe traits can become trait objects, generic methods make them not object safe.
+//! To overcome this limitation, we can use `dyn Any` as parameters to take generic arguments
+//! from non-generic methods, and call the generic methods in them.
+//! We can inspect `TypeId`s from the 'dyn Ayn's, but we can't know concrete types from the 'TypeId's.
+//! So we're going to inject functions calling generic methods with the concrete types,
+//! and invoke those functions according to the `TypeId`s.
+//! It's unsafe, extremely verbose and inefficient but I couldn't come up with any other solutions.
+//!
+//! ## Reference
+//!
+//! https://github.com/dtolnay/erased-serde/blob/master/explanation/main.rs
+
+use core::mem::{swap, zeroed};
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+    fmt::Debug,
+};
+
+/// Trait bounds for the generic methods.
+/// 'static is the only one that should be included in this pattern.
+trait Element: 'static + Debug {}
+
+/// Our target.
+trait Generic {
+    fn generic_writes<E: Element>(&mut self, fn_table: Option<&FnTable>, input: &mut E);
+    fn generic_reads<E: Element>(&mut self, fn_table: Option<&FnTable>, output: &mut E);
+}
+
+/// Generic erased.
+/// We're going to make a trait object based on this.
+trait ErasedGeneric {
+    fn erased(&mut self, fn_table: Option<&FnTable>, input: &mut dyn Any);
+}
+
+/// impl for exposure of generic methods from trait object.
+/// This is the first call on call stack.
+impl Generic for dyn ErasedGeneric {
+    #[inline]
+    fn generic_writes<E: Element>(&mut self, fn_table: Option<&FnTable>, input: &mut E) {
+        self.erased(fn_table, input as &mut dyn Any);
+    }
+
+    #[inline]
+    fn generic_reads<E: Element>(&mut self, fn_table: Option<&FnTable>, output: &mut E) {
+        self.erased(fn_table, output as &mut dyn Any);
+    }
+}
+
+/// This is the second call on call stack.
+impl ErasedGeneric for Handler {
+    #[inline]
+    fn erased(&mut self, fn_table: Option<&FnTable>, input: &mut dyn Any) {
+        // Acquires `TypeId` from the `dyn Any` and gets an appropriate delegator.
+        let ty_id = (input as &dyn Any).type_id();
+        let delegator = fn_table
+            .expect("fn_table must be filled.")
+            .get(&ty_id)
+            .expect("fn_table doesn't have appropriate entry.");
+
+        // `delegator' calls the real generic method in it.
+        (delegator)(self, input);
+    }
+}
+
+/// A struct for implementing on the trait `Target`.
+struct Handler {
+    v: Vec<Box<dyn Any>>, // Anonymous Vec
+}
+
+/// Implementation of generic piece of `Target`.
+/// This is the third and final call on call stack.
+impl Generic for Handler {
+    fn generic_writes<E: Element>(&mut self, _fn_table: Option<&FnTable>, input: &mut E) {
+        // Simple and unsafe writing test.
+        // Make sure to keep your code as safe as possible.
+        let mut dummy: E = unsafe { zeroed() };
+        swap(&mut dummy, input);
+        let input = dummy;
+        self.v.push(Box::new(input));
+
+        println!("generic_writes() got an object of {:?}", TypeId::of::<E>());
+    }
+
+    fn generic_reads<E: Element>(&mut self, _fn_table: Option<&FnTable>, output: &mut E) {
+        // Following reading test.
+        let mut elem = self.v.pop().expect("There's no elements stacked.");
+        if let Some(casted) = elem.downcast_mut::<E>() {
+            swap(casted, output);
+        }
+
+        println!("generic_reads() got an object of {:?}", TypeId::of::<E>());
+    }
+}
+
+/// This is a literally function table.
+/// We can call a specific funtion using `TypeId` from the `dyn Any`.
+/// Each function in this table calls the real generic method.
+type FnTable = HashMap<TypeId, Box<dyn Fn(&mut Handler, &mut dyn Any)>>;
+
+/// Helper macro to insert an entry into `FnTable`.
+macro_rules! add_delegator {
+    ($fn_table:ident, $handler:ty, $generic_ty:ty, $generic_fn:ident) => {
+        $fn_table.insert(
+            std::any::TypeId::of::<$generic_ty>(),
+            Box::new(|handler: &mut $handler, value: &mut dyn Any| {
+                handler.$generic_fn(None, value.downcast_mut::<$generic_ty>().unwrap());
+            }),
+        );
+    };
+}
+
+/// Test type A
+#[derive(Debug, PartialEq)]
+struct A {
+    _a1: u8,
+    _a2: u8,
+}
+
+/// Test type B
+#[derive(Debug, PartialEq)]
+struct B {
+    _b1: u16,
+}
+
+/// 'static and other bounds.
+impl Element for A {}
+impl Element for B {}
+
+fn main() {
+    // Construct a trait object.
+    let handler = Handler { v: Vec::new() };
+    let mut trait_object: Box<dyn ErasedGeneric> = Box::new(handler);
+
+    // `fn_table` has delegators to call the real generic methods.
+    let mut fn_table_writes = FnTable::new();
+    let mut fn_table_reads = FnTable::new();
+    add_delegator!(fn_table_writes, Handler, A, generic_writes);
+    add_delegator!(fn_table_writes, Handler, B, generic_writes);
+    add_delegator!(fn_table_reads, Handler, A, generic_reads);
+    add_delegator!(fn_table_reads, Handler, B, generic_reads);
+
+    // Writes something.
+    trait_object.generic_writes(Some(&fn_table_writes), &mut A { _a1: 1, _a2: 2 });
+    trait_object.generic_writes(Some(&fn_table_writes), &mut B { _b1: 3 });
+
+    // Reads back.
+    let mut a_read = A { _a1: 0, _a2: 0 };
+    let mut b_read = B { _b1: 0 };
+    trait_object.generic_reads(Some(&fn_table_reads), &mut b_read);
+    trait_object.generic_reads(Some(&fn_table_reads), &mut a_read);
+    assert_eq!(A { _a1: 1, _a2: 2 }, a_read);
+    assert_eq!(B { _b1: 3 }, b_read);
+
+    println!("Type A's id: {:?}", TypeId::of::<A>());
+    println!("Type B's id: {:?}", TypeId::of::<B>());
+}
