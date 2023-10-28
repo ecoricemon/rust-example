35bc6819281a1bed6db5ce5c4362a9c5b5396476
ecoricemon <ecoricemon@gmail.com>
2023-10-28 15:10:47 +0900

    fix: generic-trait-object: Remove irony of Generic and FnTable

--- generic-trait-object/src/main.rs
+++ generic-trait-object/src/main.rs
@@ -27,78 +27,20 @@ use std::{
 };
 
 /// Trait bounds for the generic methods.
-/// 'static is the only one that should be included in this pattern.
+/// 'static must be included to use `dyn Any`.
 trait Element: 'static + Debug {}
 
 /// Our target.
 trait Generic {
-    fn generic_writes<E: Element>(&mut self, fn_table: Option<&FnTable>, input: &mut E);
-    fn generic_reads<E: Element>(&mut self, fn_table: Option<&FnTable>, output: &mut E);
+    fn generic_writes<E: Element>(&mut self, param: &mut E);
+    fn generic_reads<E: Element>(&mut self, param: &mut E);
 }
 
 /// Generic erased.
 /// We're going to make a trait object based on this.
 trait ErasedGeneric {
-    fn erased(&mut self, fn_table: Option<&FnTable>, input: &mut dyn Any);
-}
-
-/// impl for exposure of generic methods from trait object.
-/// This is the first call on call stack.
-impl Generic for dyn ErasedGeneric {
-    #[inline]
-    fn generic_writes<E: Element>(&mut self, fn_table: Option<&FnTable>, input: &mut E) {
-        self.erased(fn_table, input as &mut dyn Any);
-    }
-
-    #[inline]
-    fn generic_reads<E: Element>(&mut self, fn_table: Option<&FnTable>, output: &mut E) {
-        self.erased(fn_table, output as &mut dyn Any);
-    }
-}
-
-/// This is the second call on call stack.
-impl ErasedGeneric for Handler {
-    #[inline]
-    fn erased(&mut self, fn_table: Option<&FnTable>, input: &mut dyn Any) {
-        // Acquires `TypeId` from the `dyn Any` and gets an appropriate delegator.
-        let ty_id = (input as &dyn Any).type_id();
-        let delegator = fn_table
-            .expect("fn_table must be filled.")
-            .get(&ty_id)
-            .expect("fn_table doesn't have appropriate entry.");
-
-        // `delegator' calls the real generic method in it.
-        (delegator)(self, input);
-    }
-}
-
-/// A struct for implementing on the trait `Target`.
-struct Handler {
-    v: Vec<Box<dyn Any>>, // Anonymous Vec
-}
-
-/// This is the third and final call on call stack.
-impl Generic for Handler {
-    fn generic_writes<E: Element>(&mut self, _fn_table: Option<&FnTable>, input: &mut E) {
-        // Simple and unsafe writing test.
-        // Make sure to keep your code as safe as possible.
-        let mut dummy: E = unsafe { zeroed() };
-        swap(&mut dummy, input);
-        let input = dummy;
-        self.v.push(Box::new(input));
-
-        println!("generic_writes() got an object of {:?}", TypeId::of::<E>());
-    }
-
-    fn generic_reads<E: Element>(&mut self, _fn_table: Option<&FnTable>, output: &mut E) {
-        // Following reading test.
-        let mut elem = self.v.pop().expect("There's no elements stacked.");
-        if let Some(casted) = elem.downcast_mut::<E>() {
-            swap(casted, output);
-        }
-
-        println!("generic_reads() got an object of {:?}", TypeId::of::<E>());
-    }
+    fn erased_writes(&mut self, param: &mut dyn Any);
+    fn erased_reads(&mut self, param: &mut dyn Any);
 }
 
 /// This is a literally function table.
@@ -106,13 +48,91 @@ impl Generic for Handler {
 /// Each function in this table calls the real generic method.
 type FnTable = HashMap<TypeId, Box<dyn Fn(&mut Handler, &mut dyn Any)>>;
 
+/// An implementation of `Generic` and `ErasedGeneric`.
+struct Handler {
+    // Tables will be taken in `impl ErasedGeneric` so that they are type of `Option`.
+    fn_table_writes: Option<FnTable>,
+    fn_table_reads: Option<FnTable>,
+    v: Vec<Box<dyn Any>>, // Anonymous Vec
+}
+
+/// impl for exposure of generic methods from trait object.
+/// This is the first call on call stack.
+impl Generic for dyn ErasedGeneric {
+    #[inline]
+    fn generic_writes<E: Element>(&mut self, param: &mut E) {
+        self.erased_writes(param as &mut dyn Any);
+    }
+
+    #[inline]
+    fn generic_reads<E: Element>(&mut self, param: &mut E) {
+        self.erased_reads(param as &mut dyn Any);
+    }
+}
+
+/// This is the second call on call stack.
+impl ErasedGeneric for Handler {
+    #[inline]
+    fn erased_writes(&mut self, param: &mut dyn Any) {
+        let ty_id = (param as &dyn Any).type_id();
+        let fn_table = self
+            .fn_table_writes
+            .take()
+            .expect("fn_table_writes must be filled.");
+        let delegator = fn_table
+            .get(&ty_id)
+            .expect("fn_table_writes doesn't have appropriate entry.");
+        (delegator)(self, param);
+        self.fn_table_writes = Some(fn_table); // Gives it back.
+    }
+
+    #[inline]
+    fn erased_reads(&mut self, param: &mut dyn Any) {
+        let ty_id = (param as &dyn Any).type_id();
+        let fn_table = self
+            .fn_table_reads
+            .take()
+            .expect("fn_table_reads must be filled.");
+        let delegator = fn_table
+            .get(&ty_id)
+            .expect("fn_table_reads doesn't have appropriate entry.");
+        (delegator)(self, param);
+        self.fn_table_reads = Some(fn_table);
+    }
+}
+
+/// Real implementations for the trait `Generic`.
+/// This is the third and final call on call stack.
+impl Generic for Handler {
+    fn generic_writes<E: Element>(&mut self, param: &mut E) {
+        // Simple and unsafe writing test.
+        // Make sure to keep your code as safe as possible.
+        let mut dummy: E = unsafe { zeroed() };
+        swap(&mut dummy, param);
+        let input = dummy;
+        self.v.push(Box::new(input));
+
+        println!("generic_writes() got an object of {:?}", TypeId::of::<E>());
+    }
+
+    fn generic_reads<E: Element>(&mut self, param: &mut E) {
+        // Following reading test.
+        let mut elem = self.v.pop().expect("There's no elements stacked.");
+        if let Some(casted) = elem.downcast_mut::<E>() {
+            swap(casted, param);
+        }
+
+        println!("generic_reads() got an object of {:?}", TypeId::of::<E>());
+    }
+}
+
 /// Helper macro to insert an entry into `FnTable`.
 macro_rules! add_delegator {
     ($fn_table:ident, $handler:ty, $generic_ty:ty, $generic_fn:ident) => {
         $fn_table.insert(
             std::any::TypeId::of::<$generic_ty>(),
             Box::new(|handler: &mut $handler, value: &mut dyn Any| {
-                handler.$generic_fn(None, value.downcast_mut::<$generic_ty>().unwrap());
+                handler.$generic_fn(value.downcast_mut::<$generic_ty>().unwrap());
             }),
         );
     };
@@ -136,10 +156,6 @@ impl Element for A {}
 impl Element for B {}
 
 fn main() {
-    // Construct a trait object.
-    let handler = Handler { v: Vec::new() };
-    let mut trait_object: Box<dyn ErasedGeneric> = Box::new(handler);
-
     // `fn_table` has delegators to call the real generic methods.
     let mut fn_table_writes = FnTable::new();
     let mut fn_table_reads = FnTable::new();
@@ -148,15 +164,23 @@ fn main() {
     add_delegator!(fn_table_reads, Handler, A, generic_reads);
     add_delegator!(fn_table_reads, Handler, B, generic_reads);
 
+    // Constructs a trait object.
+    let handler = Handler {
+        fn_table_writes: Some(fn_table_writes),
+        fn_table_reads: Some(fn_table_reads),
+        v: Vec::new(),
+    };
+    let mut trait_object: Box<dyn ErasedGeneric> = Box::new(handler);
+
     // Writes something.
-    trait_object.generic_writes(Some(&fn_table_writes), &mut A { _a1: 1, _a2: 2 });
-    trait_object.generic_writes(Some(&fn_table_writes), &mut B { _b1: 3 });
+    trait_object.generic_writes(&mut A { _a1: 1, _a2: 2 });
+    trait_object.generic_writes(&mut B { _b1: 3 });
 
     // Reads back.
     let mut a_read = A { _a1: 0, _a2: 0 };
     let mut b_read = B { _b1: 0 };
-    trait_object.generic_reads(Some(&fn_table_reads), &mut b_read);
-    trait_object.generic_reads(Some(&fn_table_reads), &mut a_read);
+    trait_object.generic_reads(&mut b_read);
+    trait_object.generic_reads(&mut a_read);
     assert_eq!(A { _a1: 1, _a2: 2 }, a_read);
     assert_eq!(B { _b1: 3 }, b_read);
 
